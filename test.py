69a8c2eb37049a54e635395d1989164b83a30545
307adi <35955705+307adi@users.noreply.github.com>
2021-06-14 23:40:24 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,21 @@
+
+import unittest
+#from cal.py import ops
+ops = {"+": (lambda x,y: x+y), "-": (lambda x,y: x-y), "*": (lambda x,y: x*y), "/": (lambda x,y: x/y)}
+
+class Test(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(ops["+"](1,3), 4, "Should be 4")
+
+    def test_sub(self):
+        self.assertEqual(ops["-"](5,3), 2, "Should be 2")
+
+    def test_mul(self):
+        self.assertEqual(ops["*"](1,3), 3, "Should be 3")
+
+    def test_div(self):
+        self.assertEqual(ops["/"](6,3), 2, "Should be 2")
+
+if __name__ == '__main__':
+    unittest.main()
